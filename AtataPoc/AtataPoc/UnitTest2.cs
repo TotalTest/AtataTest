7beba761e4dcd9bfe758593d73255a8063247d1d
Prab <prab@totaltestltd.co.uk>
2020-12-22 13:04:16 +0000

    Testing

---
+++ AtataPoc/AtataPoc/UnitTest2.cs
@@ -0,0 +1,23 @@
+using Atata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AtataPoc
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void ATest()
+        {
+            System.Console.WriteLine("Running test 1");
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void BTest()
+        {
+            System.Console.WriteLine("Running test 2");
+            Assert.AreEqual(3, 4);
+        }
+    }
+}
